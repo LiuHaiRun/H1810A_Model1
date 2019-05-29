1e01c196631ce776f9dc867de984ded14de54093
LiuHaiRun <1014884337@qq.com>
2019-05-29 15:49:42 +0800

    第一次

---
+++ app/src/main/java/com/example/liuhairui/day01_text01/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.liuhairui.day01_text01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Hello World!
+     */
+    private TextView mTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTv = (TextView) findViewById(R.id.tv);
+    }
+}
